75acf769f77e6e0733ddaef65aacfae405e1b9c3
rohitcolinrao <factintheory@gmail.com>
2020-03-11 19:27:52 -0400

    week 2

---
+++ js/week2.js
@@ -0,0 +1,30 @@
+//variables (let, const) can be strings, numbers, functions, array, object
+//functions
+
+// TYPES
+// string = a list of characters 'strung' together
+// number = 1
+
+
+let arr = [
+  "hi there", 
+  "hey", 
+  "what is up my dude", 
+  "bye"
+] 
+
+let obj = {
+  ro:  "hi there",
+  gil: "bye",
+  eric: "hey"
+}
+
+let x = (gil2) => {
+
+  console.log( arr[0] )
+  console.log( obj.eric )
+  console.log( obj[gil2] )
+
+}
+
+x("gil")
