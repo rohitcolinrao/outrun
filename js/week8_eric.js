f0b2f3b4c2e53a020b3c7a47ea0c20a51a5c53d3
rohitcolinrao <factintheory@gmail.com>
2020-04-19 23:09:24 -0400

    updates

--- js/week8_eric.js
+++ js/week8_eric.js
@@ -17,3 +17,47 @@ router.post('/:collectionType/:id/comments', function(req, res, next) {
     res.status(500).send("Error posting comment");
   })
 })
+
+
+/* get comments */
+router.get('/:collectionType/:id/comments', function(req, res, next) {
+  let commentsRef = db.collection(req.params.collectionType).doc(req.params.id).collection("comments")
+  let getComments = commentsRef
+  .orderBy("timestamp", "asc")
+  .get()
+  .then((snapshot) => {
+    let res_data = []
+    let formattedDate
+    if(snapshot.docs.length > 0){
+      snapshot.forEach((doc) => {
+        let formatted_date = doc.data().timestamp.toDate()
+        res_data.push({
+          id: doc.id,
+          ...doc.data(),
+          timestamp: formatted_date
+        })
+      });
+      res.status(200).send(res_data);
+    }
+    else{
+      //console.log('no comments!');
+      res.status(204).send("no comments");
+    }
+  })
+  .catch(err => {
+    res.status(500).send("Error getting comments docs");
+  })
+})
+
+
+
+
+
+// firebase-service.js
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+
+module.exports = admin
